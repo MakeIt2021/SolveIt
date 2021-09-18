9fc7a848332ba7e3cc51042e6a80be34956371a6
JustTest <69149187+Talub07@users.noreply.github.com>
2021-09-18 18:29:25 +0500

    Constraints...

--- app/src/main/java/org/makeitapps/solveit/MainActivity.java
+++ app/src/main/java/org/makeitapps/solveit/MainActivity.java
@@ -11,46 +11,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton settings;
-    View everything;
-    ImageButton thanks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        settings = findViewById(R.id.settingsBtn);
-        everything = findViewById(R.id.everything);
-        thanks = findViewById(R.id.thanks);
+    
+    }
 
-        if (getIntent().getBooleanExtra("wasChanged", false) == true) {
-            Snackbar snackbar = Snackbar.make(everything,R.string.success, Snackbar.LENGTH_SHORT);
-            snackbar.setBackgroundTint(Color.parseColor("#00b86b"));
-            snackbar.setTextColor(Color.WHITE);
-            snackbar.show();
+    void toSum() {
 
-        }
-        Intent intent = new Intent(this, Solving.class);
-        Intent intent1 = new Intent(this, SettingsActivity.class);
-        Intent intent2 = new Intent(this, ThanksActivity.class);
-        everything.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intent);
-            }
-        });
-        settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intent1);
-            }
-        });
+    }
 
-        thanks.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intent2);
-            }
-        });
-
+    void toCustom() {
+        // TODO: 9/18/2021 Сделать кастомные настройки! 
     }
 }
